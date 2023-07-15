1f20cc1136102ccf627cd7aa6f538d00de51a7e9
Abe Katsuhisa <katsuhisa.abe@nifty.com>
2023-07-15 15:50:48 +0900

    AtCoder Boot Camp for Beginners Easy 010 B. B - Card Game for Two - Rust

---
+++ Easy/010/rust/src/main.rs
@@ -0,0 +1,12 @@
+use proconio::input;
+fn main() {
+    input! {
+        n: usize,
+        mut a: [i32; n]
+    }
+    a.sort_by(|aa, bb| bb.cmp(aa));
+    println!("{:?}", a.iter().enumerate().map( |(index, &val)| {
+        return if index % 2 == 0 { val } else { - val };
+    }).collect::<Vec<i32>>().iter().sum::<i32>());
+
+}
